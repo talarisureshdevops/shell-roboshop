f6ae7f56be3071b29b37aeaeffd6da6b3376e66c
talarisureshdevops <TalariSuresh.career@email.com>
2026-03-01 19:28:48 +0530

    added

--- shipping.sh
+++ shipping.sh
@@ -1,31 +1,33 @@
-#!/bin/bash/
-USER_ID=$(id -u)
+#!/bin/bash
+
+USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-MYSQL_HOST="mysql.laddudevops86.fun"
-SCRIPT_DIR=$PWD
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.daws88s.online
 
-if [ $USER_ID -ne 0 ]; then 
-echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
 fi
+
 mkdir -p $LOGS_FOLDER
 
-validate(){
-if [ $1 -ne 0 ]; then
-echo -e "$2...........................$R Failure $N" | tee -a $LOGS_FILE
-exit 1
-else 
-echo -e "$2...........................$G Success $N" | tee -a $LOGS_FILE
-fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
 }
 
-dnf install maven -y
-validate $? "installing maven"
+dnf install maven -y &>>$LOGS_FILE
+VALIDATE $? "Installing Maven"
 
 id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
@@ -36,38 +38,29 @@ else
 fi
 
 mkdir -p /app 
-validate $? "create the directory"
+VALIDATE $? "Creating app directory"
 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
-validate $? "download the shipping code"
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip  &>>$LOGS_FILE
+VALIDATE $? "Downloading shipping code"
 
-cd /app &>>$LOGS_FILE
-validate $? "Moving to app directory"
+cd /app
+VALIDATE $? "Moving to app directory"
 
-rm -rf /app/* &>>$LOGS_FILE
-validate $? "removing existing code"
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 
 unzip /tmp/shipping.zip &>>$LOGS_FILE
-validate $? "Unzip shipping code"
+VALIDATE $? "Uzip shipping code"
 
-mvn clean package 
-validate $? "Packages downloading"
+cd /app 
+mvn clean package &>>$LOGS_FILE
+VALIDATE $? "Installing and Building shipping"
 
 mv target/shipping-1.0.jar shipping.jar 
-validate $? "Rename the shipping file"
-
+VALIDATE $? "Moving and Renaming shipping"
 
 cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
 VALIDATE $? "Created systemctl service"
-
-systemctl daemon-reload
-validate $? "daemon-reload"
-
-systemctl enable shipping 
-validate $? "enable-shipping"
-
-systemctl start shipping
-validate $? "start shipping"
 
 dnf install mysql -y  &>>$LOGS_FILE
 VALIDATE $? "Installing MySQL"
